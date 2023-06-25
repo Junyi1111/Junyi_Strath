d2f87285c18bbec5980d168ec3e867bbe6cc6b39
bstephen <bstephen@users.noreply.github.com>
2023-06-25 17:11:24 +0100

    Intraday error function added

--- benchmark/tao_vanilla_model.py
+++ benchmark/tao_vanilla_model.py
@@ -8,11 +8,15 @@ from datetime import datetime
 from typing import List
 import pickle
 
+#vanilla linear benchmark - proposed by T. Hong for GefCom14
+#eventually will have superclass that generalises this functionality
+#author: J. Lu, B.Stephen
 class tao_vanilla_model:
     def __init__(self) -> None:
         self.taovanilla = linear_model.LinearRegression()
-        self.resolution = 48#assume 48 HH in day 
-        self.horizon = self.resolution#and its one day ahead 
+        self.resolution = 48#assume 48 HH in day - actually irrelevant for this model
+        self.horizon = self.resolution#and its one day ahead - again not relevant here
+        self.label = ''
 
     def __init__(self,res) -> None:
         self.taovanilla = linear_model.LinearRegression()
@@ -20,7 +24,7 @@ class tao_vanilla_model:
         self.horizon = self.resolution
     
     def __str__(self) -> str:
-        return 'Linear Regression Benchmark Model'
+        return self.label+'Linear Regression Benchmark Model'
 
     @classmethod
     def load_model_from_disk(cls,fname):
@@ -59,16 +63,6 @@ class tao_vanilla_model:
     def hour_of_year(dt): 
         return dt.hour+dt.dayofyear*24
     
-    def form_lagged_target(load,lags):
-        Y=load
-        T=Y.shape[0]
-
-        for lag in lags:
-            Y=np.delete(Y,slice(T-lag-1,T-1),axis=0)
-            Y=np.concat(Y,load[lag:])
-
-        return Y
-
     def form_vanilla_covariates(hrTrnd,ambT,hod,mod,mon,dow):
         X=np.array([hrTrnd,dow*hod,mon,mon*ambT,mon*ambT*ambT,mon*ambT*ambT*ambT,hod*ambT,hod*ambT*ambT,hod*ambT*ambT*ambT]).T
 
@@ -79,44 +73,58 @@ class tao_vanilla_model:
         mod=np.array(timestamp.minute)
         mon=np.array(timestamp.month)
         dow=np.array(timestamp.dayofweek)
-        #new_format = "%d-%b-%Y %H:%M:%S"
-        #hour_trend=tao_vanilla_model.hour_trend_of_year(timestamp,new_format)
         hour_trend=tao_vanilla_model.hour_of_year(timestamp)
 
+        #need to do various checks here - timestamp validity
+        #lengths of covariate sequences etc
+
         X=tao_vanilla_model.form_vanilla_covariates(hour_trend,temperature,hod,mod,mon,dow)
-        Y=tao_vanilla_model.form_lagged_target(load,[self.horizon])
+        Y=load
 
-        (T,d)=X.shape
-
-        X=np.concat(X[:T-self.horizon],Y[:T-self.horizon,1])
-        Y=Y[1+self.horizon:,2]
-
+        #this is all there is to it - for a date, time and temperature estimate
+        #do a point prediction of load
         self.taovanilla.fit(X,Y)
 
         return
     
-    def evaluate_error_stats(self,Y,Yhat):
+    #make this a base class method - retained for
+    #basic probabilistic forecasting
+    def evaluate_error_stats(self,tstamp,Y,Yhat):
+        inc=1440//self.resolution#increment
+        mul=self.resolution//24#hour multiplier
+        idx=(mul*ts.hour+ts.minute//inc)
+        mxIdx=max(idx)
         error=Y-Yhat
+        
+        self.muIntraday=np.array((self.resolution,1),dtype=float)
+        self.sigIntraday=np.array((self.resolution,self.resolution),dtype=float)
 
-        #next thing to do is turn the error into a mean and covariance
-        blockErr=np.reshape(error[0:-self.horizon-1],(-1,self.horizon))
-        mu=np.mean(blockErr,axis=0)
-        sigma=np.cov(blockErr,rowvar=False)
+        for i in range(1,mxIdx):
+            self.muIntraday[i-1]=np.mean(error[idx==i],axis=0)
+            self.sigIntraday[i-1][i-1]=np.var(error[idx==i],axis=0)
 
-        return mu,sigma
+            for j in range(1,mxIdx):
+                cov=np.cov(error[idx==i],error[idx==j])
+                self.sigIntraday[i-1][j-1]=cov
+                self.sigIntraday[j-1][i-1]=cov
 
-    def forecast(self,timestamp,temperature,load):
-        return self.forecast(timestamp,temperature,load,self.horizon)
+        ##not general - assumes a 48 HH resolution...
+        #blockErr=np.reshape(error[0:-self.horizon-1],(-1,self.horizon))
+        #mu=np.mean(blockErr,axis=0)
+        #sigma=np.cov(blockErr,rowvar=False)
 
-    def forecast(self,timestamp,temperature,load,horizon):
+        return self.muIntraday,self.sigIntraday
+
+    def forecast(self,timestamp,temperature):
+        return self.forecast(timestamp,temperature,self.horizon)
+
+    def forecast(self,timestamp,temperature,horizon):
         #to do - if horizon is multiple of existing model - apply recursively
         #if not, apply recursively and interpolate
-        hod=np.array(timestamp.hour)
+        hod=np.array(timestamp.hour)#roll these forward for increased horizons
         mod=np.array(timestamp.minute)
         mon=np.array(timestamp.month)
         dow=np.array(timestamp.dayofweek)
-        #new_format = "%d-%b-%Y %H:%M:%S"
-        #hour_trend=tao_vanilla_model.hour_trend_of_year(timestamp,new_format)
         hour_trend=tao_vanilla_model.hour_of_year(timestamp)
         
         X=tao_vanilla_model.form_vanilla_covariates(hour_trend,temperature,hod,mod,mon,dow)
@@ -125,23 +133,27 @@ class tao_vanilla_model:
         
         return yHat
 
+    #todo: include methods for rolling forward timestamps
+    #and also increasing forecast resolution...
+
 flxnet=pd.read_csv('flex_networks.csv')
 
 taovanilla = tao_vanilla_model(48)
 
 ts=pd.DatetimeIndex(flxnet.Timestamp)
 ld=flxnet.kinnessPark_F4
-ld2=flxnet.kinnessPark_F2
+ts2=pd.DatetimeIndex(flxnet.Timestamp)
+testTarget=flxnet.kinnessPark_F2
 aT=flxnet['Air Temperature']
 
 taovanilla.train_model(ts,aT,ld)
 taovanilla.save_model_to_disk('flex_networks_stlf.pkl')
 
-targetHat=taovanilla.forecast(ts,aT,ld2)
+taovanilla2=tao_vanilla_model.load_model_from_disk('flex_networks_stlf.pkl')
 
-error=ld2-targetHat
+targetHat=taovanilla.forecast(ts2,aT,0)#wrong! won't recognise overloaded method though...
 
 #next thing to do is turn the error into a mean and covariance
-muErr,sigErr=taovanilla.evaluate_error_stats(testTarget,targetHat)
+muErr,sigErr=taovanilla.evaluate_error_stats(ts2,testTarget,targetHat)
 
 #estimate intra-day error using conditional Gaussian form of joint error
