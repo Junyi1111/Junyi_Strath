0668e44db3af237bfef4d4614a858d0318610390
Junyi_strath <junyi.lu@strath.ac.uk>
2024-04-24 22:10:18 +0100

    Add files via upload

--- benchmark/SubstationDistanceAnalyzer.py
+++ benchmark/SubstationDistanceAnalyzer.py
@@ -59,11 +59,11 @@ class SubstationDistanceAnalyzer:
                 path_based_distances.append((row['SPADDR1'], None))
         return pd.DataFrame(path_based_distances, columns=['Address', 'Path Distance (m)'])
 
-    def visualize_network(self, num_substations=12):
-        fig, ax = plt.subplots(figsize=(10, 10))
-        self.hv_cable.plot(ax=ax, color='lightgray', linewidth=1, zorder=1, label="HV Cable")
+    def visualize_network(self, num_substations=22):
+        fig, ax = plt.subplots(figsize=(6, 6))
+        self.hv_cable.plot(ax=ax, color='lightgray', linewidth=1, zorder=1, label="11kV Cable")
         ax.scatter(self.sub_point.x, self.sub_point.y, color='red', s=150, edgecolor='k', zorder=5,
-                   label='Primary Substation', marker='o')
+                   label='33kV Primary Substation', marker='o')
         secondary_label_added = False
         plotted_substations = 0
         for idx, row in self.closest_substations.iterrows():
@@ -73,8 +73,8 @@ class SubstationDistanceAnalyzer:
             try:
                 shortest_path_nodes = nx.shortest_path(self.G, source=self.nearest_source_node,
                                                        target=nearest_target_node, weight='length')
-                ax.scatter(row['geometry'].x, row['geometry'].y, color='blue', s=100,
-                           label='Secondary Substation' if not secondary_label_added else "", zorder=4, marker='o')
+                ax.scatter(row['geometry'].x, row['geometry'].y, color='blue', s=50,
+                           label='11kV Secondary Substation' if not secondary_label_added else "", zorder=4, marker='o')
                 secondary_label_added = True
                 path_lines = []
                 for u, v in zip(shortest_path_nodes[:-1], shortest_path_nodes[1:]):
@@ -83,16 +83,16 @@ class SubstationDistanceAnalyzer:
                 plotted_substations += 1
             except nx.NetworkXNoPath:
                 continue
-        ax.set_title("18 Battlefield Road substation")
-        ax.legend(loc="upper left")
+        ax.legend(loc="upper right", fontsize=10, prop={'family': 'Times New Roman'})
         ax.set_xticks([])
         ax.set_yticks([])
         ax.set_xticklabels([])
         ax.set_yticklabels([])
-        ax.text(self.sub_point.x + 1200, self.sub_point.y - 1600, "|--- 100m ---|", ha='center', va='center',
-                fontsize=9, fontweight='bold', color='black')
+        ax.text(self.sub_point.x + 600, self.sub_point.y - 800, "|--- 100m ---|", ha='center', va='center',
+                fontsize=10, fontweight='bold', color='black',fontname="Times New Roman")
         ax.plot([self.sub_point.x + 1100, self.sub_point.x + 1300], [self.sub_point.y - 1700, self.sub_point.y - 1700],
                 color='black', linewidth=2)
-        ax.set_xlim(self.sub_point.x - 2000, self.sub_point.x + 2000)
-        ax.set_ylim(self.sub_point.y - 2000, self.sub_point.y + 2000)
+        ax.set_xlim(self.sub_point.x - 1000, self.sub_point.x + 1000)
+        ax.set_ylim(self.sub_point.y - 1000, self.sub_point.y + 1000)
+        plt.savefig('network_visualization.svg', dpi=300, bbox_inches='tight')
         plt.show()
